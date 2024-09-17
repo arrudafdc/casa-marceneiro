46c167b1978db403dec72e393d09f91cd6d88e71
arrudafdc <arrudafdc@gmail.com>
2024-09-17 15:09:53 -0300

    add new photos for gallery

--- src/pages/Gallery/index.jsx
+++ src/pages/Gallery/index.jsx
@@ -4,24 +4,30 @@ import { GalleryContainer, GalleryGrid } from "./styles";
 import img1 from "/assets/gallery/img1.jpg";
 import img2 from "/assets/gallery/img2.jpg";
 import img3 from "/assets/gallery/img3.jpg";
+import img4 from "/assets/gallery/img4.jpg";
+import img5 from "/assets/gallery/img5.jpg";
+import img6 from "/assets/gallery/img6.jpg";
+import img7 from "/assets/gallery/img7.jpg";
+import img8 from "/assets/gallery/img8.jpg";
+import img9 from "/assets/gallery/img9.jpg";
+import img10 from "/assets/gallery/img10.jpg";
+import img11 from "/assets/gallery/img11.jpg";
+import img12 from "/assets/gallery/img12.jpg";
 import "yet-another-react-lightbox/styles.css";
 import React from "react";
 
 const photos = [
-  { src: img1 },
+  { src: img7 },
+  { src: img11 },
   { src: img2 },
-  { src: img3 },
-  { src: img2 },
+  { src: img4 },
+  { src: img5 },
+  { src: img6 },
   { src: img1 },
-  { src: img3 },
-  { src: img3 },
-  { src: img2 },
-  { src: img1 },
-  { src: img1 },
-  { src: img3 },
-  { src: img2 },
-  { src: img1 },
-  { src: img2 },
+  { src: img8 },
+  { src: img9 },
+  { src: img10 },
+  { src: img12 },
   { src: img3 },
 ];
 
