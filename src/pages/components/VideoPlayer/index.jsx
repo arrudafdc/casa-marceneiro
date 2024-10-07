8a6fd4dc53ad7b554be4646f96792e4f702b2e7c
arrudafdc <arrudafdc@gmail.com>
2024-10-07 18:23:07 -0300

    add src for video

--- src/pages/components/VideoPlayer/index.jsx
+++ src/pages/components/VideoPlayer/index.jsx
@@ -1,12 +1,12 @@
 import { VideoContainer, VideoSection } from "./styles";
-// import videoFile from "/assets/video.mp4";
+import videoFile from "/assets/main-video.mp4";
 import thumb from "/assets/thumb-video.jpg";
 
 export function VideoPlayer() {
   return (
     <VideoContainer>
       <VideoSection>
-        {/* <video poster={thumb} controls src={videoFile}></video> */}
+        <video poster={thumb} controls src={videoFile}></video>
       </VideoSection>
     </VideoContainer>
   );
