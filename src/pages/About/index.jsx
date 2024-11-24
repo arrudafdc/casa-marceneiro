cddbd1341b3bcb1e68b17fdd47ba3048ef39dffe
arrudafdc <arrudafdc@gmail.com>
2024-11-24 10:56:11 -0300

    create more information in about section

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -2,14 +2,24 @@ import React from "react";
 import { MainContainer } from "../../styles/global";
 import AboutBg from "/assets/bg-about.jpg";
 import logoIcon from "/assets/logo-icon.svg";
+import bgFesta from "/assets/casa-sobre.jpg";
+import banheiro from "/assets/banheiro.jpg";
+import cozinha from "/assets/cozinha.jpg";
+import arco from "/assets/arco-flores.jpg";
+import decoracao from "/assets/decoracao.jpg";
 
 import AOS from "aos";
 import "aos/dist/aos.css";
 import {
   AboutAccommodations,
   AccommodationContentContainer,
+  AccommodationContentSection,
+  HouseFullAboutContent,
+  HouseFullAboutContentTwo,
+  HouseFullParhAboutContent,
+  HouseFullParhAboutContentTwo,
   MainAboutSection,
-  Teste,
+  TwoImagemSection,
 } from "./styles";
 
 export function About() {
@@ -18,56 +28,98 @@ export function About() {
   }, []);
 
   return (
-    <MainContainer>
-      <MainAboutSection>
-        <h1 data-aos="fade-up">
-          Seu refúgio conta com <span>4.100 m² de área total</span>
-        </h1>
-        <div>
-          <img src={AboutBg} />
-        </div>
-      </MainAboutSection>
-      <AboutAccommodations>
-        <h2>Comodidades para seu evento</h2>
-        <Teste>
-          <AccommodationContentContainer>
-            <img src={logoIcon} />
-            <p>
-              Área coberta construída de 390m² com piso de cimento queimado e
-              estrutura de eucalipto
-            </p>
-          </AccommodationContentContainer>
+    <>
+      <MainContainer>
+        <MainAboutSection>
+          <h1 data-aos="fade-up">
+            Seu refúgio conta com <span>4.100 m² de área total</span>
+          </h1>
+          <div>
+            <img src={AboutBg} />
+          </div>
+        </MainAboutSection>
+        <AboutAccommodations>
+          <h2>
+            Nossos Serviços <span>& Comodidades</span>
+          </h2>
+          <AccommodationContentSection>
+            <AccommodationContentContainer>
+              <img src={logoIcon} />
+              <p>
+                Área coberta construída de 390m² com piso de cimento queimado e
+                estrutura de eucalipto
+              </p>
+            </AccommodationContentContainer>
 
-          <AccommodationContentContainer>
-            <img src={logoIcon} />
-            <p>
-              Banheiros Climatizados Masculino e Feminino com capacidade para
-              até 150 pessoas
-            </p>
-          </AccommodationContentContainer>
-          <AccommodationContentContainer>
-            <img src={logoIcon} />
-            <p>
-              Espaço para cozinha interna e externa, ideal para atender às
-              necessidades do evento
-            </p>
-          </AccommodationContentContainer>
-          <AccommodationContentContainer>
-            <img src={logoIcon} />
-            <p>
-              Estrutura completa para staff com banheiros para equipes e
-              estacionamento de serviço
-            </p>
-          </AccommodationContentContainer>
-          <AccommodationContentContainer>
-            <img src={logoIcon} />
-            <p>
-              Um de nossos anfitriões para atender e auxiliar no que for
-              necessário durante todo o evento
-            </p>
-          </AccommodationContentContainer>
-        </Teste>
-      </AboutAccommodations>
-    </MainContainer>
+            <AccommodationContentContainer>
+              <img src={logoIcon} />
+              <p>
+                Banheiros Climatizados Masculino e Feminino com capacidade para
+                até 150 pessoas
+              </p>
+            </AccommodationContentContainer>
+            <AccommodationContentContainer>
+              <img src={logoIcon} />
+              <p>
+                Espaço para cozinha interna e externa, ideal para atender às
+                necessidades do evento
+              </p>
+            </AccommodationContentContainer>
+            <AccommodationContentContainer>
+              <img src={logoIcon} />
+              <p>
+                Estrutura completa para staff com banheiros para equipes e
+                estacionamento de serviço
+              </p>
+            </AccommodationContentContainer>
+            <AccommodationContentContainer>
+              <img src={logoIcon} />
+              <p>
+                Um de nossos anfitriões para atender e auxiliar no que for
+                necessário durante todo o evento
+              </p>
+            </AccommodationContentContainer>
+          </AccommodationContentSection>
+        </AboutAccommodations>
+      </MainContainer>
+
+      <HouseFullAboutContent>
+        <HouseFullParhAboutContent>
+          <p data-aos="fade-right">
+            Nossa Casa está aberta para te receber em outros formatos! Seja um
+            casamento intimista ou grande, seja uma festa de welcome ou after, e
+            até mesmo aniversários.
+          </p>
+        </HouseFullParhAboutContent>
+        <div>
+          <img src={bgFesta} />
+        </div>
+      </HouseFullAboutContent>
+
+      <HouseFullAboutContentTwo>
+        <div>
+          <img src={cozinha} />
+        </div>
+        <HouseFullParhAboutContentTwo>
+          <p data-aos="fade-right">
+            Nossa Casa está aberta para te receber em outros formatos! Seja um
+            casamento intimista ou grande.
+          </p>
+          <div>
+            <img src={banheiro} />
+          </div>
+        </HouseFullParhAboutContentTwo>
+      </HouseFullAboutContentTwo>
+      <MainContainer>
+        <TwoImagemSection>
+          <div data-aos="fade-right">
+            <img src={arco} alt="" />
+          </div>
+          <div data-aos="fade-left">
+            <img src={decoracao} alt="" />
+          </div>
+        </TwoImagemSection>
+      </MainContainer>
+    </>
   );
 }
--- src/pages/About/styles.js
+++ src/pages/About/styles.js
@@ -59,6 +59,9 @@ export const AboutAccommodations = styled.section`
     h2 {
       font-size: 1rem;
     }
+    span {
+      display: block;
+    }
   }
 
   @media (max-width: 800px) {
@@ -75,9 +78,105 @@ export const AccommodationContentContainer = styled.div`
   align-items: center;
 `;
 
-export const Teste = styled.div`
+export const AccommodationContentSection = styled.div`
   display: flex;
   flex-wrap: wrap;
   justify-content: space-around;
   gap: 3rem;
+`;
+
+export const HouseFullAboutContent = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  margin-bottom: 7rem;
+
+  img {
+    width: 100%;
+  }
+
+  @media (max-width: 1000px) {
+    grid-template-columns: 1fr;
+    gap: 5rem;
+    margin-bottom: 5rem;
+
+    & > div:first-child {
+      order: 2;
+    }
+
+    & > div:last-child {
+      order: 1;
+    }
+  }
+`;
+
+export const HouseFullParhAboutContent = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  p {
+    font-size: 1.25rem;
+    max-width: 370px;
+  }
+
+  @media (max-width: 1000px) {
+    p {
+      padding: 0 1rem;
+    }
+  }
+`;
+
+export const HouseFullAboutContentTwo = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  margin-bottom: 7rem;
+
+  img {
+    width: 100%;
+    display: block;
+  }
+
+  @media (max-width: 1000px) {
+    grid-template-columns: 1fr;
+  }
+`;
+
+export const HouseFullParhAboutContentTwo = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background: #c9bdab;
+  gap: 2rem;
+
+  p {
+    font-size: 1.25rem;
+    max-width: 310px;
+    color: #fff;
+    text-align: center;
+  }
+
+  @media (max-width: 1000px) {
+    & {
+      padding: 5rem 0;
+    }
+  }
+`;
+
+export const TwoImagemSection = styled.div`
+  display: flex;
+  gap: 4rem;
+  margin-bottom: 10rem;
+
+  img {
+    width: 100%;
+  }
+
+  @media (max-width: 1000px) {
+    & {
+      flex-direction: column;
+      margin-bottom: 5rem;
+      gap: 2rem;
+    }
+  }
 `;
